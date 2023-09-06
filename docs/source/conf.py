2171677f7e43917327aa2c23e8b4a050eb9ff467
anonymouslojz <13428981281@163.com>
2023-09-06 15:55:14 +0800

    add docs.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ release = '0.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx_markdown_tables']
+extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = []
