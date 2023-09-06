151458463cafc20bbe248a4eeb80d0e0e3432d4e
anonymouslojz <13428981281@163.com>
2023-09-06 15:33:17 +0800

    add docs.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,8 +5,6 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-$ python
-import sphinx_rtd_theme
 
 project = 'pibiger'
 copyright = '2023, joe'
