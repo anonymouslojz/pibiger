33d62d2224b5cefcb73fca4dcde8776a43c1cfd9
anonymouslojz <13428981281@163.com>
2023-09-06 16:29:03 +0800

    add docs.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,45 +3,18 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-
-###
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-import os
-import sys
-import shlex
-sys.path.insert(0, os.path.abspath('.'))
-
-import recommonmark
-from recommonmark.transform import AutoStructify
-
-source_suffix = ['.rst', '.md']
-master_doc = 'index'
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
 project = 'pibiger'
 copyright = '2023, joe'
 author = 'joe'
 release = '0.0'
+
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.mathjax',
-    'recommonmark',
-    'sphinx_markdown_tables',]
+extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -51,5 +24,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
 html_static_path = ['_static']
